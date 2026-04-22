294449c7f2fb2290cba257441798722653a4263f
Haron036 <haronkipkirui2@gmail.com>
2026-04-22 11:00:37 +0300

    changes

--- Backend/GlobalMarketplace/src/main/java/RotechMarketplace/globalmarketplace/Entities/Product.java
+++ Backend/GlobalMarketplace/src/main/java/RotechMarketplace/globalmarketplace/Entities/Product.java
@@ -47,7 +47,7 @@ public class Product {
     private Double pickupLongitude;
     // ─────────────────────────────────────────────────────────────────────────
 
-    @ElementCollection
+    @ElementCollection(fetch = FetchType.EAGER)
     @CollectionTable(name = "product_images", joinColumns = @JoinColumn(name = "product_id"))
     @Column(name = "image_url", length = 1000)
     private List<String> images;
